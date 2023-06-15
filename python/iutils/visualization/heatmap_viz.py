aeeae3b1aaffc64c101ab74340c6a4f482387b62
Philipp Schröppel <schroepp@cs.uni-freiburg.de>
2023-06-15 17:45:33 +0200

    Remove requirement to load libiutils.so for heatmap visualization

--- python/iutils/visualization/heatmap_viz.py
+++ python/iutils/visualization/heatmap_viz.py
@@ -8,28 +8,8 @@
 ### --------------------------------------- ###
 
 def heatmap_viz(data, min, max):
-    from numpy.ctypeslib import ndpointer
-    import ctypes
-    import numpy as np
-
-    lib = ctypes.cdll.LoadLibrary('libiutils.so')
-
-    c_heat_map_viz = lib.heatmap_viz
-    c_heat_map_viz.restype = None
-    c_heat_map_viz.argtypes = [ndpointer(ctypes.c_float, flags='C_CONTIGUOUS'),
-                               ndpointer(ctypes.c_ubyte, flags='C_CONTIGUOUS'),
-                               ctypes.c_int,
-                               ctypes.c_int,
-                               ctypes.c_float,
-                               ctypes.c_float]
-
-    cont_data = np.ascontiguousarray(data, np.float32)
-    height, width = data.shape[0], data.shape[1]
-
-    result = np.zeros((height, width, 3), dtype=np.uint8)
-
-    c_heat_map_viz(cont_data, result, width, height, min, max)
-
+    from .vis import vis_2d_array
+    result = vis_2d_array(data, out_format={'type': 'np'}, clipping=True, upper_clipping_thresh=max, lower_clipping_thresh=min,).transpose([1, 2, 0])
     return result
 
 
